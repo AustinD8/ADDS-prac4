35db0317020b06b842d68c24845beb68bfab6bd0
AustinD8 <88066285+AustinD8@users.noreply.github.com>
2022-04-04 12:52:59 +0930

    added reverse

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "Reverse.h"
+#include <iostream>
+
+int main(){
+    Reverse revFunction=Reverse();
+    int num = 13248572;
+    std::string word="hello";
+    std::cout<<revFunction.reverseString(word)<<std::endl;
+
+
+
+}
